6a223ff74f7c8fba4329d870efe34eb2982f4ce2
ingra14m <1261238025@qq.com>
2022-12-20 13:26:25 +0800

    Remove redundent website

--- src/app.py
+++ src/app.py
@@ -5,15 +5,6 @@ import re
 
 sys.path.append("..")
 
-# db = pymysql.connect(
-#     host='sh-cdb-1xzdh3ty.sql.tencentcdb.com',
-#     port=60557,
-#     user='root',
-#     passwd='Asdf0506@',
-#     db='CBA_Final'
-#
-# )
-
 app = Flask(__name__)
 
 
@@ -23,64 +14,12 @@ def index():
 
 
 # 球队的网页
-
-
-# @app.route('/team/<name>')
-# def team(name):
-#     cursor = db.cursor()
-#     nameInSql = '"' + name + '"'
-#     #
-#     # sql_Team = '''
-#     #         select `姓名`, `出场数`,`总得分`/`出场数`,`总篮板` /`出场数`,`进攻篮板`/`出场数`,`防守篮板`/`出场数`,`助攻`/`出场数`,`抢断`/`出场数`,`盖帽`/`出场数` \
-#     #         ,`失误`/`出场数`,`犯规`/`出场数`
-#     #         from basic2019
-#     #         where `球队` = %s;
-#     # '''% (nameInSql)
-#
-#
-#     sql_Team = '''
-#             select `姓名`, `出场数`,(`两分命中数` * 2 + `三分命中数` * 3 + `罚球命中数`)/`出场数`,(`进攻篮板` + `防守篮板`) /`出场数`,`进攻篮板`/`出场数`,`防守篮板`/`出场数`,`助攻`/`出场数`,`抢断`/`出场数`,`盖帽`/`出场数` \
-#             ,`失误`/`出场数`,`犯规`/`出场数`
-#             from basic2019
-#             where `球队` = %s;
-#     '''% (nameInSql)
-#
-#     cursor.execute(sql_Team)
-#
-#     information = []
-#     alldata = cursor.fetchall()
-#
-#     for data in alldata:
-#         information.append(data)
-#
-#
-#     db.commit()
-#
-#     # for i in range(len(information)):
-#     #     print(information[i])
-#     cursor.close()
-#
-#     for i in range(len(information)):
-#         information[i] = list(information[i])
-#
-#     for player in information:
-#         print(player)
-#
-#     factor = [['姓名','出场数','得分','篮板','进攻篮板','防守篮板','助攻','抢断','盖帽','失误','犯规']]
-#     information = factor + information
-#     print(name)
-#     print(information)
-#
-#     return render_template("team.html",name = name, information = information)
-
-
 @app.route('/team/<name>')
 def team(name):
     db = pymysql.connect(
         user='cba2020',
         passwd='cba2020',
-        db='cba2020'
-
+        db='cba'
     )
     cursor = db.cursor()
     nameInSql = '"' + name + '"'
@@ -107,7 +46,6 @@ def team(name):
     for data in alldata:
         information.append(data)
 
-    # print(information)
     db.commit()
     cursor.execute(sql_Team_Infor)
 
@@ -119,28 +57,20 @@ def team(name):
     introduce[0] = list(introduce[0])
     print(introduce)
 
-    # for i in range(len(information)):
-    #     print(information[i])
     cursor.close()
 
     for i in range(len(information)):
         information[i] = list(information[i])
-
-    # for player in information:
-    #     if (re.search("Decimal") != -1):
-    #         information[player.index] = re.findall("Decimal\('(.*?)'\)",player)
 
     for i in range(len(information)):
         for j in range(2, len(information[i])):
             information[i][j] = round(float(information[i][j]), 2)
 
     factor = [["姓名", '出场数', '得分', '篮板', '进攻篮板', '防守篮板', '助攻', '抢断', '盖帽', '失误', '犯规']]
-    # print(factor)
     information = factor + information
 
     db.close()
     return render_template("player-statics.html", name=name, information=information, introduce=introduce)
-    # return render_template("player-statics.html")
 
 
 # 球员
@@ -174,15 +104,6 @@ def player(Name, player):
             alldata2 = [alldata2[0]]
             message.update({str(i): alldata2})
 
-    # 查询高阶数据的页面
-    # sql_advanced = '''
-    #         select *
-    #         from basic2019
-    #         where where `球队` = %s and `姓名` = "唐正东"
-    # ''' % (NameInSQL)
-
-    # for key,value in message.items():
-    #     print(key, value, sep = "\t", end="\n")
     cursor.close()
 
     db.close()
